6dc74b62f547125547eb8599da97c85bdf07a662
Anand Sharma <anandkumarsharma93068@gmail.com>
2025-10-31 09:55:08 +0530

    navbar is ready

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Button } from '../ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { LogOut, User2 } from 'lucide-react'
+
+const Navbar = () => {
+    const user = false
+    return (
+        <div className='bg-white'>
+            <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
+                <div>
+
+                    <h1 className='text-2xl font-bold'>Job <span className='text-[#f83002]'>Portal</span></h1>
+                </div>
+                <div className='flex items-center gap-12'>
+                    <ul className='flex font-medium items-center gap-5'>
+                        <li>Home</li>
+                        <li>Job</li>
+                        <li>Browser</li>
+                    </ul>
+                    {
+                        !user ? (
+                            <div className='flex items-center gap-2'>
+                                <Link to={"/login"}><Button variant={"outline"}>Login</Button></Link>
+                                <Link to={"/signup"}><Button className={"bg-[#6a38c2] hover:bg-[#310975]"}>SignIn</Button></Link>
+                            </div>
+                        ) : (
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <Avatar className={"cursor-pointer"}>
+                                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                    </Avatar>
+                                </PopoverTrigger>
+                                <PopoverContent className={"w-80"}>
+                                    <div className='space-y-2'>
+                                        <Avatar className={"cursor-pointer"}>
+                                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                        </Avatar>
+                                        <div>
+                                            <h4 className='font-medium'>Anand Sharma</h4>
+                                            <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit amet.</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex flex-col text-gray-600'>
+                                        <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                                            <User2 />
+                                            <Button variant={"link"}>View Profile</Button>
+                                        </div>
+                                        <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                                            <LogOut />
+                                            <Button variant={"link"}>Logout</Button>
+                                        </div>
+                                    </div>
+                                </PopoverContent>
+                            </Popover>
+                        )
+                    }
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
